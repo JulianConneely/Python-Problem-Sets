bf18e819e83e1a229d9a1312bd342ce677e4daa1
JulianConneely <36239852+JulianConneely@users.noreply.github.com>
2018-02-16 20:36:11 +0000

    Update fibsurname.py

--- fibsurname.py
+++ fibsurname.py
@@ -1,5 +1,7 @@
 # Julian Conneely, 2018-02-05
 # A program that displays Fibonacci numbers using people's names.
+# My surname is Conneely The first letter C is number 67 The last letter y is number 121 Fibonacci number 188 is 871347450517368352816615810882615488381
+# ord() returns an integer (or decimal) representing the Unicode code point of that character
 
 def fib(n):
   """This function returns the nth Fibonacci number."""
