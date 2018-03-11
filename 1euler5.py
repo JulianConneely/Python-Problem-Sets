2967cb28235877798d9e518145e682d5c7e76802
Conneely <jconneely@hpe.com>
2018-03-11 20:22:09 +0000

    Uploaded euler#5 problem WIP

---
+++ 1euler5.py
@@ -0,0 +1,12 @@
+# Julian Conneely 09/03/2018
+# https://projecteuler.net/problem=5
+# What is the smallest number divisible by each of the numbers 1 to 20? 
+
+i = 1 
+for k in (range(1, 21)): 
+    if i % k > 0: 
+        for j in range(1, 21): 
+            if (i*j) % k == 0: 
+                i *= j 
+                break 
+print (i)
---
+++ bin.py
@@ -0,0 +1,5 @@
+# Julian Conneely 08/03/2018
+# binary addition
+# not working
+
+bin(3)
