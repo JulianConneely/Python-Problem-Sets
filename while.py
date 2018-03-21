292b92cb112c9ebe72cb513e64b58243b787f7e0
Julian Conneely <julian_conneely@hotmail.com>
2018-03-21 20:34:56 +0000

    update comments

--- while.py
+++ while.py
@@ -1,8 +1,11 @@
 #Julian Conneely, 21/03/18
 #while lopp with increment
+
 #X is equal to 0 
-#While X is less than or equal to 20 Do print current value of X 
-#Add 2 to X Repeat condition 
+#While X is less than or equal to 20 
+#Do print current value of X 
+#Add 2 to X 
+#Repeat condition 
 
 x = 0
 while x <=20:
