2d95489d14fd452ac397071059162ca553d238bc
Julian Conneely <julian_conneely@hotmail.com>
2018-03-25 20:40:40 +0100

    1euler5

--- 1euler5.py
+++ 1euler5.py
@@ -2,6 +2,8 @@
 # https://projecteuler.net/problem=5
 # What is the smallest number divisible by each of the numbers 1 to 20? 
 
+#what is k i and j?
+
 i = 1 
 for k in (range(1, 21)): 
     if i % k > 0: 
