46ac71a38a8c73ebf5b48e5775728b6c8c033733
Julian Conneely <julian_conneely@hotmail.com>
2018-03-28 18:05:23 +0100

    update iris.py with comments

--- Iris.py
+++ Iris.py
@@ -4,6 +4,10 @@
 # With knows files, auto closes so no need to close
 
 with open("data/iris.csv") as f: # create a block of python code with f as the name of the file that is open (iris.csv)
-  for line in f:     
-    a = line.split(',')  
-    print(a[0],a[1],a[2],a[3],a[4])
+  for line in f:                 # loop through the lines in f 
+    
+    # splits out and print elements 0 to 3 of f
+    # assigns 3 spaces to each element, with 2 spaces assigned for the decimal and one for the space between columns
+    print('{:3} {:3} {:3} {:3}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))     
+     
+    
---
+++ iris_less_code.py
@@ -0,0 +1,10 @@
+# Julian Conneely 28/03/18
+# Iris data set, Exercise 5
+
+# With knows files, auto closes so no need to close
+
+with open("data/iris.csv") as f: # create a block of python code with f as the name of the file that is open (iris.csv)
+  for line in f:     
+    a = line.split(',')  
+    print(a[0],a[1],a[2],a[3])
+ 
