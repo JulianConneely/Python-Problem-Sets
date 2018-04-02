e742df7606e4cd77998283e0d68c4e64382948c8
Julian Conneely <julian_conneely@hotmail.com>
2018-04-02 17:16:35 +0100

    test

--- Iris.py
+++ Iris.py
@@ -2,6 +2,7 @@
 # Iris data set, Exercise 5
 
 # With knows files, auto closes so no need to close
+# 
 
 with open("data/iris.csv") as f: # create a block of python code with f as the name of the file that is open (iris.csv)
   for line in f:                 # loop through the lines in f 
