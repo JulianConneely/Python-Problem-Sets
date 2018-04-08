c44f448e9cdcc5474871f06c78cac11c7236ff7d
JulianConneely <36239852+JulianConneely@users.noreply.github.com>
2018-04-08 13:48:29 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -11,6 +11,6 @@ def factorial(i):           #factorial is the function name and i is the argumen
     return number           #Not part of the while loop (indentation); return number and continue the loop while i>0, then close the loop
 
 # three seperate calls to the defined function
-print("The factorial of #5 is:",factorial(5))       # call the factorial function with 5 as the value i
-print("The factorial of #7 is:",factorial(7))       # call the factorial function with 7 as the value i
-print("The factorial of #10 is:",factorial(10))     # call the factorial function with 10 as the value i
+print("The factorial of #5 is:",factorial(5))       # calls the factorial function with 5 as the value i
+print("The factorial of #7 is:",factorial(7))       # calls the factorial function with 7 as the value i
+print("The factorial of #10 is:",factorial(10))     # calls the factorial function with 10 as the value i
