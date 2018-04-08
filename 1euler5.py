1c95d3132a380332559c8daa310ea83dcbe0b608
Julian Conneely <julian_conneely@hotmail.com>
2018-04-08 15:17:19 +0100

    update euler comments

--- 1euler5.py
+++ 1euler5.py
@@ -2,13 +2,12 @@
 # https://projecteuler.net/problem=5
 # What is the smallest number divisible by each of the numbers 1 to 20? 
 
-#what is k i and j?
 
 i = 1 
-for k in (range(1, 21)): 
-    if i % k > 0: 
-        for j in range(1, 21): 
-            if (i*j) % k == 0: 
-                i *= j 
+for k in (range(1, 21)):        # the FOR loop iterates over the sequence of numbers inisde the range 1 to 20
+    if i % k > 0:               # if the remainder of i divide by k is greater than 0
+        for j in range(1, 21):  #
+            if (i*j) % k == 0:  #
+                i *= j          # 
                 break 
 print (i)
--- euler5.py
+++ euler5.py
@@ -5,11 +5,12 @@
 i=20
 while 1:
     i+=20   # is equivalent to i = i + 20
-    # Any integer evenly divisible by 20 is evenly divisible by 2 
-    # (but the reverse might not be true). So we leave the 20 and take out the 2, the 4, and the 5
-    # Leave the 19, as it's prime. Leave the 18, but now we can take out the 3 and the 6
-    # If you repeat this process, you wind up with a much shorter list of numbers to try
-    if i%11==0 and i%13==0 and i%14==0 and i%16==0 \
-        and i%17==0 and i%18==0 and i%19==0:
+
+    # starting with i=20 beacuse the asn must be divisible by 20
+    # Any integer evenly divisible by 18 is also evenly divisible by 2 9 6 and 3 etc so we can exclude these etc
+    # Leave prime numbers like 19 17 13 11
+    # 7 covered by 14 etc
+    # So we repeat this process ending up with a much shorter list of numbers to try
+    if i%11==0 and i%13==0 and i%14==0 and i%15==0 and i%16==0 and i%17==0 and i%18==0 and i%19==0:
         print (i)
         break
